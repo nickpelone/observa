9ef371cb668e30eccaa55b38c2c21286be016525
Nick Pelone <npelone@unca.edu>
2015-03-04 11:22:17 -0500

    Initial working demo of signaling and client side video capture.

--- static/client.js
+++ static/client.js
@@ -1,5 +1,42 @@
 (function () {
     "use strict";
+    // cross browser compat shim
+    navigator.getUserMedia = navigator.getUserMedia ||
+    navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
     console.log("Hello world!");
+
+    var videoConstaints = {video: true};
+
+    function onMediaOpen(localMediaStream) {
+        window.stream = localMediaStream;
+        var video = $("#basic_webrtc_demo_video")[0];
+        video.src = window.URL.createObjectURL(localMediaStream);
+        video.play(); // start playback
+    }
+
+    function onMediaError(error) {
+        console.log("navigator.getUserMedia error: " + error);
+    }
+
+    //grab the user media object and pass it to onMediaOpen
+    $("#start_demo_one").click(function () {
+        navigator.getUserMedia(videoConstaints, onMediaOpen, onMediaError);
+    });
+
+    $("#start_demo_two").click(function () {
+        var video = $("#basic_webrtc_demo_video")[0];
+        var newVideoSource = "http://atv-demo.nickpelone.com/yt/keyboard-cat.mp4";
+        video.src = newVideoSource;
+        video.play();
+    });
+    $("#pause_play_button").click(function () {
+        var video = $("#basic_webrtc_demo_video")[0];
+
+        if(video.paused) {
+            video.play();
+        } else {
+            video.pause();
+        }
+    });
 }());
