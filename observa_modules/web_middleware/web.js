ea40e296412a9f902669ef1ec7c23be627137c76
Nick Pelone <nick.pelone@calyptix.com>
2015-03-16 13:02:23 -0400

    Rename austin-tv -> Observa. Git endpoint changed to nickpelone/observa.git

---
+++ observa_modules/web_middleware/web.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+/* This is not the file to launch Observa!
+ * See src/app.js
+ */
+var exports = module.exports = {};
+
+exports.startWebInterface = function (port) {
+    var express = require('express');
+    var eApp = express();
+    
+    eApp.get('/', function(request, response) {
+        response.send("hello");
+    });
+    
+    eApp.listen(port, function () {
+        console.log("Web Interface is up");
+    });
+    
+};
+
+
