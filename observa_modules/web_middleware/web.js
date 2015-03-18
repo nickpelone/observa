ed282950e24bf8788726d33f3385ff1d28cc9d0d
Nick Pelone <npelone@unca.edu>
2015-03-18 17:37:24 -0400

    Ask the web middleware module to serve the web client

--- observa_modules/web_middleware/web.js
+++ observa_modules/web_middleware/web.js
@@ -6,11 +6,9 @@ var exports = module.exports = {};
 
 exports.startWebInterface = function (port) {
     var express = require('express');
+    var path = require('path');
     var eApp = express();
-    
-    eApp.get('/', function(request, response) {
-        response.send("hello");
-    });
+    eApp.use(express.static(path.join(__dirname,"../../static")));
     
     eApp.listen(port, function () {
         console.log("Web Interface is up on port " + port);
