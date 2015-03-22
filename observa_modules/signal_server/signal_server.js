faca77296df689705ea2e231093a4c2b7fee7626
Nick Pelone <npelone@unca.edu>
2015-03-22 17:10:26 -0400

    Skelleton code for signaling server

---
+++ observa_modules/signal_server/signal_server.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env/node 
+
+// This contains the signaling server functions and related
+
+var exports = module.exports = {};
+
+exports.StartSignalServer = function (port) {
+
+    console.log("Signal Server up on " + port);
+
+};
+
+
