a7d22f1d7368b69d101d7ec946314c91dcba0b21
Nick Pelone <npelone@unca.edu>
2015-03-22 17:28:15 -0400

    Skelleton code for In-Call UI Handling and event firing

---
+++ static/js/call-client.js
@@ -0,0 +1,41 @@
+(function () {
+    "use strict";
+
+    //declarations
+    //
+
+    function startup() {
+        console.log("startup() was called");
+    }
+
+    function startCall() {
+        console.log("startCall() was called");
+    }
+
+    function end() {
+        console.log("end() was called");
+    }
+
+    var localVideoStream, localPeerConnection, remotePeerConnection;
+
+    var localVideoElement = $("#local_video")[0];
+    var remoteVideoElement = $("#remote_video")[0];
+
+    var startButton = $("#start_button")[0];
+    var callButton = $("#call_button")[0];
+    var endButton = $("#end_button")[0];
+
+    //set default interface states
+
+    startButton.disabled = false;
+    callButton.disabled = true;
+    endButton.disabled = true;
+
+    // click handlers
+    $("#start_button").click(startup);
+    $("#call_button").click(startCall);
+    $("#end_button").click(end);
+
+
+
+})();
