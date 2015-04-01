6f11d16849b86c674ea046a9bfd71b8d836dc213
Nick Pelone <npelone@unca.edu>
2015-04-01 11:01:17 -0400

    implement call function for a local stream

--- static/call/call-client.js
+++ static/call/call-client.js
@@ -5,29 +5,6 @@
     //declarations
     //
 
-    function successCallback(videoStream) {
-        console.log("successCallback() was called");
-    }
-
-    function startup() {
-        console.log("startup() was called");
-        console.log("Requesting local video stream...");
-        startButton.disabled = true;
-        var constraints = {};
-
-        getUserMedia(constraints, successCallback, function(error) {
-            console.log("error: " + error);
-        });
-    }
-
-    function startCall() {
-        console.log("startCall() was called");
-    }
-
-    function end() {
-        console.log("end() was called");
-    }
-
     var localVideoStream, localPeerConnection, remotePeerConnection;
 
     var localVideoElement = $("#local_video")[0];
@@ -36,6 +13,62 @@
     var startButton = $("#start_button")[0];
     var callButton = $("#call_button")[0];
     var endButton = $("#end_button")[0];
+
+    function successCallback(videoStream) {
+        console.log("successCallback() was called");
+    }
+
+    function errorCallback(error) {
+        console.log(error);
+    }
+
+    function startup() {
+        console.log("startup() was called");
+        console.log("Requesting local video stream...");
+        startButton.disabled = true;
+        var constraints = {};
+
+        getUserMedia(constraints, successCallback, function (error) {
+            console.log("error: " + error);
+        });
+    }
+
+    function startCall() {
+        console.log("startCall() was called");
+        callButton.disabled = true;
+        hangupButton.disabled = false;
+
+        if (localVideoStream.getVideoTracks().length > 0) {
+            // if there are available local video tracks
+            console.log("using video device: " + localVideoStream.getVideoTracks()[0].label);
+        }
+        if (localVideoStream.getAudioTracks().length > 0) {
+            //if there are available local audio tracks
+            //TODO: allow user to pick their own
+            console.log("Using audio device: " + localVideoStream.getVideoTracks[0].label);
+        }
+
+        var observaServers = null;
+        localPeerConnection = new RTCPeerConnection(servers);
+        console.log("Created local peer connection object localPeerConnection");
+        localPeerConnection.onicecandidate = gotLocalIceCandidate;
+
+        remotePeerConnection = new RTCPeerConnection(servers);
+        console.log("Created remote peer connection object remotePeerConnection");
+        remotePeerConnection.onicecandidate = gotRemoteIceCandidate;
+        remotePeerConnection.onaddstream = gotRemoteStream;
+
+        localPeerConnection.addStream(localStream);
+        console.log("Added localStream to localPeerConnection");
+        localPeerConnection.createOffer(gotLocalDescription, errorCallback);
+
+    }
+
+    function end() {
+        console.log("end() was called");
+    }
+
+
 
     //set default interface states
 
