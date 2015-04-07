5f019ca9120a6b946edfff06b82a17cdcbd1ddae
Nick Pelone <npelone@unca.edu>
2015-04-07 19:32:45 -0400

    Have the web middleware mirror the JSON file from Google

--- observa_modules/web_middleware/web.js
+++ observa_modules/web_middleware/web.js
@@ -8,8 +8,22 @@ exports.startWebInterface = function (port) {
     var express = require('express');
     var path = require('path');
     var eApp = express();
+    var request = require('request');
     eApp.use(express.static(path.join(__dirname,"../../static")));
     
+    eApp.get('/turn-servers', function (request, response) {
+        //get turn servers from google
+        var url = "https://computeengineondemand.appspot.com/turn?username=41784574&key=4080218913";
+        request({url: url,
+                 json: true,
+                }, function (error, response, body) {
+                    if (!error && response.statusCode === 200) {
+                        response.setHeader('Content-Type', 'application/json');
+                        response.end(body);
+                    }
+        });
+    });
+
     eApp.listen(port, function () {
         console.log("Web Interface is up on port " + port);
     });
