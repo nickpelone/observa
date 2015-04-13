1d654c611ef57e5f87736df3d9158803d1ea5531
Nick Pelone <npelone@unca.edu>
2015-04-13 16:16:16 -0400

    STUN only

--- static/call/libObservaRTC.js
+++ static/call/libObservaRTC.js
@@ -13,10 +13,7 @@ var localStream, remoteStream, peerConnection;
 //peer connection options, use google STUN for NAT traversal
 //todo: auto-magically specify TURN as well
 var pcConfig = {
-    'iceServers': [
-                   {'url':'turn:observa.nickpelone.com:3478',
-'credential': 'pleasework',
-'user': 'observa'}]
+    'iceServers': [{'url':'stun:stun.services.mozilla.com'}]
 };
 
 //peer connection constraints
