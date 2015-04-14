3a2c88c6268b8dbbde14b1120c8285c8d40808fd
Nick Pelone <npelone@unca.edu>
2015-04-14 16:10:53 -0400

    Typo in child_process

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var observaWeb = require("./observa_modules/web_middleware/web.js");
 var observaSignalServer = require("./observa_modules/signal_server/new_signal_server.js");
 
 /* clean the temporary folder Observa uses for plugin content */
-var exec = require('child-process').exec;
+var exec = require('child_process').exec;
 
 var tmpCleaner = exec("rm -rfd /tmp/observa", function (error, stdout, stderr) {
     if(error) throw error;
