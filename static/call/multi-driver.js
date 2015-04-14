6e26006916fa997251e12995ce79f47ea4b0e878
Nick Pelone <npelone@unca.edu>
2015-04-14 14:29:02 -0400

    A first pass at video switching in-call

--- static/call/multi-driver.js
+++ static/call/multi-driver.js
@@ -1,6 +1,32 @@
 var connection = new RTCMultiConnection().connect();
+var backupLocalVideoSrc, backupRemoteVideoSrc;
+
+function changeObservaVideoSource(video, target) {
+    if (target === 'local') {
+        /* replace the local video stream with the received video */
+        var localVideoElement = $("#local_video")[0];
+        backupLocalVideoSrc = localVideoElement.src;
+        localVideoElement.src = video;
+
+        /* send the message to the other clients to load the video as well */
+        var pluginMsg = {
+            'message': 'plugin',
+            'video': video
+        };
+        connection.sendCustomMessage(pluginMsg);
+    } else if (target === 'remote') {
+        /* we are changing the remote's video to the video from the message */
+        var remoteVideoElement = $("#remote_video")[0];
+        backupRemoteVideoSrc = remoteVideoElement.src;
+        remoteVideoElement.src = video;
+    }
+}
 connection.onCustomMessage = function (message) {
     console.log("received a custom message: " + message);
+    if (message.message === 'plugin') {
+        /* received a video from another client to play */
+        changeObservaVideoSource(message.video, 'remote');
+    }
 };
 
 $("#start_button").click(function (event) {
@@ -25,6 +51,7 @@ $("#plugin_button").click(function (event) {
         success: function(data) {
             console.log("Successfully posted");
             console.log(data);
+            changeObservaVideoSource(data.video, 'local');
         }
     });
 });
