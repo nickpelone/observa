982aef6d21858b8f1b9467a7d962a832bc51c674
Nick Pelone <npelone@unca.edu>
2015-04-14 16:59:53 -0400

    Add stub function for click handler for broadcast button

--- static/launcher.js
+++ static/launcher.js
@@ -4,4 +4,7 @@
          var channel_desired = $("#channelName").val();
          window.location.href = "/call/incall-ui.html?call=" + channel_desired;
     });
+    $("#startBroadcast").click(function (event) {
+        alert("This isn't implemented yet!");
+    });
 }());
