311226c4c94c279e6d49ed6cbc6f50c2b20f0532
Nick Pelone <npelone@unca.edu>
2015-04-14 15:56:03 -0400

    Try to fix the sizing when you're sending a video, so it appears as your webcam output

--- static/call/multi-driver.js
+++ static/call/multi-driver.js
@@ -5,11 +5,13 @@ function changeObservaVideoSource(video, target) {
     if (target === 'local') {
         $(".local_video").toggle();
         $("#plugin_content_area").toggle();
+        $("#plugin_content_area").toggleClass('local_video');
         pluginArea.src = video;
         pluginArea.play(); //force it to play on the local side
         pluginArea.onended = function () {
             $(".local_video").toggle();
             $("#plugin_content_area").toggle();
+            $("#plugin_content_area").toggleClass('local_video');
         };
 
         /* send the message to the other clients to load the video as well */
