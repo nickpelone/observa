ee9c83bfff30e38ededc73e461ca54555396b737
Nick Pelone <npelone@unca.edu>
2015-04-14 16:09:04 -0400

    Clean the Observa temporary folder on each launch

--- app.js
+++ app.js
@@ -3,6 +3,15 @@
 var observaWeb = require("./observa_modules/web_middleware/web.js");
 var observaSignalServer = require("./observa_modules/signal_server/new_signal_server.js");
 
+/* clean the temporary folder Observa uses for plugin content */
+var exec = require('child-process').exec;
+
+var tmpCleaner = exec("rm -rfd /tmp/observa", function (error, stdout, stderr) {
+    if(error) throw error;
+    if(stderr) console.log(stderr);
+});
+
+
 observaWeb.startWebInterface(80);
 observaSignalServer.startSignalServer(1234);
 
