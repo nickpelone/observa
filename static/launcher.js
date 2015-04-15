cbf91b53c7ea65bbf397d31bb9567ec360033243
Nick Pelone <npelone@unca.edu>
2015-04-15 12:54:01 -0400

    Enable the broadcasting feature at the launcher page.

--- static/launcher.js
+++ static/launcher.js
@@ -1,10 +1,9 @@
 (function () {
-
+    var channel_desired = $("#channelName").val();
     $("#startCall").click(function (event) {
-         var channel_desired = $("#channelName").val();
          window.location.href = "/call/incall-ui.html?call=" + channel_desired;
     });
     $("#startBroadcast").click(function (event) {
-        alert("This isn't implemented yet!");
+        window.location.href = "/broadcast/broadcaster-ui.html?broadcast=" + channel_desired;
     });
 }());
