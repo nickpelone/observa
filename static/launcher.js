6e85b1c9cdb09fbc0a37b6927083d58b62ce3368
Nick Pelone <npelone@unca.edu>
2015-04-15 14:38:08 -0400

    Just define it for each handler, as it global seems to be weird

--- static/launcher.js
+++ static/launcher.js
@@ -1,9 +1,11 @@
 (function () {
-    var channel_desired = $("#channelName").val();
+
     $("#startCall").click(function (event) {
+        var channel_desired = $("#channelName").val();
          window.location.href = "/call/incall-ui.html?call=" + channel_desired;
     });
     $("#startBroadcast").click(function (event) {
+        var channel_desired = $("#channelName").val();
         window.location.href = "/broadcast/broadcaster-ui.html?broadcast=" + channel_desired;
     });
 }());
