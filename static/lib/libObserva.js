5b1e3ae5bb24a8c3f9c88f2b0381b9a1cf50284d
Nick Pelone <npelone@unca.edu>
2015-04-16 01:52:13 -0400

    Attempt to animate the transitions between video sources

--- static/lib/libObserva.js
+++ static/lib/libObserva.js
@@ -1,14 +1,17 @@
 function changeObservaVideoSource(video, target) {
+    var animationDuration = 800;
     var pluginArea = $("#plugin_content_area")[0];
     if (target === 'local') {
-        $(".local_video").hide();
-        $("#plugin_content_area").show();
+        $(".local_video").hide(animationDuration);
         $("#plugin_content_area").toggleClass('local_video');
+        $("#plugin_content_area").show(animationDuration);
+
         pluginArea.src = video;
         pluginArea.play(); //force it to play on the local side
         pluginArea.onended = function () {
-            $(".local_video").show();
-            $("#plugin_content_area").hide();
+            $("#plugin_content_area").hide(animationDuration);
+            $(".local_video").show(animationDuration);
+
         };
 
         /* send the message to the other clients to load the video as well */
@@ -18,23 +21,25 @@ function changeObservaVideoSource(video, target) {
         };
         connection.sendCustomMessage(pluginMsg);
     } else if (target === 'remote') {
-        $(".remote_video").hide();
-        $("#plugin_content_area").show();
+        $(".remote_video").hide(animationDuration);
+        $("#plugin_content_area").show(animationDuration);
         pluginArea.src = video;
         pluginArea.play(); //force it to play on the local side
         pluginArea.onended = function () {
-            $(".remote_video").show();
-            $("#plugin_content_area").hide();
+            $("#plugin_content_area").hide(animationDuration);
+            $(".remote_video").show(animationDuration);
+
         };
 
     } else if (target === 'broadcast') {
-        $(".broadcast_video").hide();
-        $("#plugin_content_area").show();
+        $(".broadcast_video").hide(animationDuration);
+        $("#plugin_content_area").show(animationDuration);
         pluginArea.src = video;
         pluginArea.play(); //force it to play on the local side
         pluginArea.onended = function () {
-            $(".broadcast_video").show();
-            $("#plugin_content_area").hide();
+            $("#plugin_content_area").hide(animationDuration);
+            $(".broadcast_video").show(nimationDuration);
+
         };
 
     }
