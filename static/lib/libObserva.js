3c9bc7c2693b76730923b600b998c075a6a3ba42
Nick <npelone@unca.edu>
2015-04-16 04:09:35 +0000

    Fix bug where video sender's plugin area was revealed after playback ended.

--- static/lib/libObserva.js
+++ static/lib/libObserva.js
@@ -8,8 +8,7 @@ function changeObservaVideoSource(video, target) {
         pluginArea.play(); //force it to play on the local side
         pluginArea.onended = function () {
             $(".local_video").toggle();
-            $("#plugin_content_area").toggle();
-            $("#plugin_content_area").toggleClass('local_video');
+            $("#plugin_content_area").hide();
         };
 
         /* send the message to the other clients to load the video as well */
