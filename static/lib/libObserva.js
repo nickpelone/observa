70ff36ce7c3db9b54c313ba51c7f2a58e4bb5eec
Nick Pelone <npelone@unca.edu>
2015-04-16 10:41:30 -0400

    Attempt to ensure state isn't corrupted on multiple video shares, add animation duration for toggles

--- static/lib/libObserva.js
+++ static/lib/libObserva.js
@@ -1,14 +1,17 @@
 function changeObservaVideoSource(video, target) {
+    var animationDuration = 800;
     var pluginArea = $("#plugin_content_area")[0];
     if (target === 'local') {
-        $(".local_video").toggle();
-        $("#plugin_content_area").toggle();
+        $(".local_video").toggle(animationDuration);
+        $("#plugin_content_area").toggle(animationDuration);
         $("#plugin_content_area").toggleClass('local_video');
         pluginArea.src = video;
         pluginArea.play(); //force it to play on the local side
         pluginArea.onended = function () {
-            $(".local_video").toggle();
-            $("#plugin_content_area").hide();
+            $("#plugin_content_area").toggleClass('local_video'); //ensure we remove local video properties from the plugin area
+            $("#plugin_content_area").toggle(animationDuration);
+            $(".local_video").toggle(animationDuration);
+
         };
 
         /* send the message to the other clients to load the video as well */
@@ -18,23 +21,23 @@ function changeObservaVideoSource(video, target) {
         };
         connection.sendCustomMessage(pluginMsg);
     } else if (target === 'remote') {
-        $(".remote_video").toggle();
-        $("#plugin_content_area").toggle();
+        $(".remote_video").toggle(animationDuration);
+        $("#plugin_content_area").toggle(animationDuration);
         pluginArea.src = video;
         pluginArea.play(); //force it to play on the local side
         pluginArea.onended = function () {
-            $(".remote_video").toggle();
-            $("#plugin_content_area").toggle();
+            $(".remote_video").toggle(animationDuration);
+            $("#plugin_content_area").toggle(animationDuration);
         };
 
     } else if (target === 'broadcast') {
-        $(".broadcast_video").toggle();
-        $("#plugin_content_area").toggle();
+        $(".broadcast_video").toggle(animationDuration);
+        $("#plugin_content_area").toggle(animationDuration);
         pluginArea.src = video;
         pluginArea.play(); //force it to play on the local side
         pluginArea.onended = function () {
-            $(".broadcast_video").toggle();
-            $("#plugin_content_area").toggle();
+            $(".broadcast_video").toggle(animationDuration);
+            $("#plugin_content_area").toggle(animationDuration);
         };
 
     }
