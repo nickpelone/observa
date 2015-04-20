f8d058ec6a17d20a8319fa2215d6a64ec3f49ea2
Nick Pelone <npelone@unca.edu>
2015-04-19 20:23:09 -0400

    Start of libObserva cleanup: use constants for string selectors; fix several 'lax' syntax errors

--- static/lib/libObserva.js
+++ static/lib/libObserva.js
@@ -1,65 +1,71 @@
 var animationDuration = 800;
 var pluginState = 'none';
+var local_video = '.local_video';
+var remote_video = '.remote_video';
+var plugin_content_area = '.plugin_content_area';
+var broadcast_video = '.broadcast_video';
+
 
 function changeObservaVideoSource(video, target) {
+    var pluginMsg;
     $("#video_container").prepend("<video class='plugin_content_area'></video>");
-    var pluginArea = $(".plugin_content_area")[0];
+    var pluginArea = $(plugin_content_area)[0];
     if (target === 'local') {
-        $(".local_video").hide(animationDuration);
-        $(".plugin_content_area").toggleClass('local_video'); //ensure the plugin area fades in with the appearance of a local video
-        $(".plugin_content_area").show(animationDuration);
+        $(local_video).hide(animationDuration);
+        $(plugin_content_area).toggleClass('local_video'); //ensure the plugin area fades in with the appearance of a local video
+        $(plugin_content_area).show(animationDuration);
 
         pluginArea.src = video;
         pluginArea.play(); //force it to play on the local side
         pluginState = 'local';
         pluginArea.onended = function () {
-            $(".plugin_content_area").toggleClass('local_video'); //ensure we remove local video properties from the plugin area
-            $(".plugin_content_area").hide(animationDuration);
-            $(".local_video").show(animationDuration);
+            $(plugin_content_area).toggleClass('local_video'); //ensure we remove local video properties from the plugin area
+            $(plugin_content_area).hide(animationDuration);
+            $(local_video).show(animationDuration);
             pluginState = 'none';
-            $(".plugin_content_area").remove();
+            $(plugin_content_area).remove();
 
         };
 
         /* send the message to the other clients to load the video as well */
-        var pluginMsg = {
+        pluginMsg = {
             'message': 'plugin',
             'video': video
         };
         connection.sendCustomMessage(pluginMsg);
     } else if (target === 'remote') {
         $("#video_container").prepend("<video class='plugin_content_area'></video>");
-        $(".remote_video").hide(animationDuration);
-        $(".plugin_content_area").toggleClass('remote_video'); //constrain the plugin content to remote_video rules
-        $(".plugin_content_area").show(animationDuration);
-        $(".local_video").css("width", "5%");
+        $(remote_video).hide(animationDuration);
+        $(plugin_content_area).toggleClass('remote_video'); //constrain the plugin content to remote_video rules
+        $(plugin_content_area).show(animationDuration);
+        $(local_video).css("width", "5%");
         pluginArea.src = video;
         pluginArea.play(); //force it to play on the local side
         pluginState = 'remote';
         pluginArea.onended = function () {
-            $(".plugin_content_area").toggleClass('remote_video'); //remove plugin content from the remote_video class before revealing remote_video
-            $(".plugin_content_area").hide(animationDuration);
-            $(".local_video").css("width", "30%");
-            $(".remote_video").show(animationDuration);
+            $(plugin_content_area).toggleClass('remote_video'); //remove plugin content from the remote_video class before revealing remote_video
+            $(plugin_content_area).hide(animationDuroation);
+            $(local_video).css("width", "30%");
+            $(remote_video).show(animationDuration);
             pluginState = 'none';
-            $(".plugin_content_area").remove();
+            $(plugin_content_area).remove();
         };
 
     } else if (target === 'broadcast') {
         $("#video_container").prepend("<video class='plugin_content_area'></video>");
-        $(".broadcast_video").hide(animationDuration);
-        $(".plugin_content_area").show(animationDuration);
+        $(broadcast_video).hide(animationDuration);
+        $(plugin_content_area).show(animationDuration);
         pluginArea.src = video;
         pluginArea.play(); //force it to play on the local side
         pluginState = 'broadcast';
         pluginArea.onended = function () {
-            $(".plugin_content_area").hide(animationDuration);
-            $(".plugin_content_area").remove();
-            $(".broadcast_video").show(animationDuration);
-            pluginState = 'none'
+            $(plugin_content_area).hide(animationDuration);
+            $(plugin_content_area).remove();
+            $(broadcast_video).show(animationDuration);
+            pluginState = 'none';
         };
         if (pluginState === 'none') {
-            var pluginMsg = {
+            pluginMsg = {
                 'message': 'plugin',
                 'video': video
             };
@@ -69,7 +75,7 @@ function changeObservaVideoSource(video, target) {
 }
 
 function endObservaPluginEarly(sideOfStream) {
-    var pluginArea = $(".plugin_content_area")[0];
+    var pluginArea = $(plugin_content_area)[0];
     if (sideOfStream === 'local') {
         /*
          * We are ending the stream on the local side.
@@ -79,11 +85,11 @@ function endObservaPluginEarly(sideOfStream) {
         if (pluginState === 'local') {
             pluginArea.pause();
             pluginArea.src = "";
-            $(".plugin_content_area").hide(animationDuration);
-            $(".plugin_content_area").toggleClass('local_video');
-            $(".plugin_content_area").remove();
+            $(plugin_content_area).hide(animationDuration);
+            $(plugin_content_area).toggleClass('local_video');
+            $(plugin_content_area).remove();
             //plugin content has been hidden and removed from the local_video class
-            $(".local_video").show(animationDuration);
+            $(local_video).show(animationDuration);
 
             //We now need to tell the remote client that it's time to end the video
             var endPluginRequest = {
@@ -101,11 +107,11 @@ function endObservaPluginEarly(sideOfStream) {
         if (pluginState === 'remote') {
             pluginArea.pause();
             pluginArea.src = "";
-            $(".plugin_content_area").hide(animationDuration);
-            $(".plugin_content_area").css("display", "none");
-            $(".plugin_content_area").remove();
-            $(".local_video").css("width", "30%");
-            $(".remote_video").show(animationDuration);
+            $(plugin_content_area).hide(animationDuration);
+            $(plugin_content_area).css("display", "none");
+            $(plugin_content_area).remove();
+            $(local_video).css("width", "30%");
+            $(remote_video).show(animationDuration);
             pluginState = 'none';
         }
     } else if (sideOfStream === 'broadcast') {
@@ -113,10 +119,10 @@ function endObservaPluginEarly(sideOfStream) {
          * Remove the plugin content area, show the broadcast video again
          */
 
-        $(".plugin_content_area").hide(animationDuration);
-        $(".plugin_content_area").css("display", "none");
-        $(".plugin_content_area").remove();
-        $(".broadcast_video").show(animationDuration);
+        $(plugin_content_area).hide(animationDuration);
+        $(plugin_content_area).css("display", "none");
+        $(plugin_content_area).remove();
+        $(broadcast_video).show(animationDuration);
         pluginState = 'none';
     }
 }
