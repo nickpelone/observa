78791c0125f310a2bc7cbc2424e219d802fc1f6c
Nick Pelone <npelone@unca.edu>
2015-04-20 18:23:25 -0400

    Clean up app.js, document, and move the dropping privs code into a try block that upon fail exits with code 1

--- app.js
+++ app.js
@@ -6,15 +6,19 @@ var observaWeb = require("./observa_modules/web_middleware/web.js");
 var exec = require('child_process').exec;
 
 var tmpCleaner = exec("rm -rfd /tmp/observa", function (error, stdout, stderr) {
-    if(error) throw error;
-    if(stderr) console.log(stderr);
+    if (error) throw error;
+    if (stderr) console.log(stderr);
 });
 
-
+/* load modules */
 observaWeb.startWebInterface(80);
 
-/* drop privs */
-var uid = parseInt(process.env.SUDO_UID);
-if(uid) process.setuid(uid);
-console.log("Observa has dropped to UID " + uid);
-
+/* drop privs back down to a normal user so Observa doesn't run as root */
+try {
+    var uid = parseInt(process.env.SUDO_UID);
+    if (uid) process.setuid(uid);
+    console.log("Observa has dropped to UID " + uid);
+} catch (e) {
+    console.log("Error in dropping privs to normal user: \n Do you have sudo rights?");
+    process.exit(1); //expose error code to underlying OS
+}
