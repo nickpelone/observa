687f506b789c51d7d0e515158e169a3b4db03c7b
Nick Pelone <npelone@unca.edu>
2015-04-19 20:40:42 -0400

    libObserva cleanup: create function setupObservaPlugin to handle common code for setting up the plugin content area.

--- static/lib/libObserva.js
+++ static/lib/libObserva.js
@@ -5,27 +5,48 @@ var remote_video = '.remote_video';
 var plugin_content_area = '.plugin_content_area';
 var broadcast_video = '.broadcast_video';
 
+function setupObservaPlugin(video, targetElement, pluginClass) {
+    targetElement.src = video;
+    targetElement.play(); //begin plugin playback
+
+    if (pluginClass === local_video) {
+        pluginState = 'local';
+        targetElement.onended = function () {
+            $(plugin_content_area).toggleClass('local_video');
+            $(plugin_content_area).hide(animationDuration);
+            $(local_video).show(animationDuration);
+            pluginState = 'none';
+            $(plugin_content_area).remove();
+        };
+    } else if (pluginClass === remote_video) {
+        pluginState = 'remote';
+        targetElement.onended = function () {
+            $(plugin_content_area).toggleClass('remote_video'); //remove plugin content from the remote_video class before revealing remote_video
+            $(plugin_content_area).hide(animationDuroation);
+            $(local_video).css("width", "30%");
+            $(remote_video).show(animationDuration);
+            pluginState = 'none';
+            $(plugin_content_area).remove();
+        };
+    }
+}
+
+
 
 function changeObservaVideoSource(video, target) {
     var pluginMsg;
     $("#video_container").prepend("<video class='plugin_content_area'></video>");
     var pluginArea = $(plugin_content_area)[0];
     if (target === 'local') {
+        /* First hide the local video stream from the client interface */
         $(local_video).hide(animationDuration);
-        $(plugin_content_area).toggleClass('local_video'); //ensure the plugin area fades in with the appearance of a local video
+        /* Now give the plugin content area the rules of a local video, as we have sent the video */
+        $(plugin_content_area).toggleClass('local_video');
+        /* Now animate the plugin content area into visibility */
         $(plugin_content_area).show(animationDuration);
 
-        pluginArea.src = video;
-        pluginArea.play(); //force it to play on the local side
-        pluginState = 'local';
-        pluginArea.onended = function () {
-            $(plugin_content_area).toggleClass('local_video'); //ensure we remove local video properties from the plugin area
-            $(plugin_content_area).hide(animationDuration);
-            $(local_video).show(animationDuration);
-            pluginState = 'none';
-            $(plugin_content_area).remove();
-
-        };
+        /* setup the plugin content area, including beginning video playback */
+        setupObservaPlugin(video, pluginArea, local_video);
 
         /* send the message to the other clients to load the video as well */
         pluginMsg = {
@@ -39,23 +60,14 @@ function changeObservaVideoSource(video, target) {
         $(plugin_content_area).toggleClass('remote_video'); //constrain the plugin content to remote_video rules
         $(plugin_content_area).show(animationDuration);
         $(local_video).css("width", "5%");
-        pluginArea.src = video;
-        pluginArea.play(); //force it to play on the local side
-        pluginState = 'remote';
-        pluginArea.onended = function () {
-            $(plugin_content_area).toggleClass('remote_video'); //remove plugin content from the remote_video class before revealing remote_video
-            $(plugin_content_area).hide(animationDuroation);
-            $(local_video).css("width", "30%");
-            $(remote_video).show(animationDuration);
-            pluginState = 'none';
-            $(plugin_content_area).remove();
-        };
+
+        setupObservaPlugin(video, pluginArea, remote_video);
 
     } else if (target === 'broadcast') {
         $("#video_container").prepend("<video class='plugin_content_area'></video>");
         $(broadcast_video).hide(animationDuration);
         $(plugin_content_area).show(animationDuration);
-        pluginArea.src = video;
+        pluginArea.sr = video;
         pluginArea.play(); //force it to play on the local side
         pluginState = 'broadcast';
         pluginArea.onended = function () {
