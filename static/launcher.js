052125d1c7ddd60728f833de3a615a3cb62f83cf
Nick Pelone <npelone@unca.edu>
2015-04-21 18:05:30 -0400

    Throw an alert and return if a user does not input a channel name

--- static/launcher.js
+++ static/launcher.js
@@ -3,10 +3,18 @@
         /* click handlers for the launcher page */
         $("#startCall").click(function (event) {
             var channel_desired = $("#channelName").val();
+            if (channel_desired === null || channel_desired === '' || channel_desired === undefined) {
+                alert("You have to put in a channel name before starting!");
+                return;
+            }
             window.location.href = "/call/incall-ui.html?call=" + channel_desired;
         });
         $("#startBroadcast").click(function (event) {
             var channel_desired = $("#channelName").val();
+            if (channel_desired === null || channel_desired === '' || channel_desired === undefined) {
+                alert("You have to put in a channel name before starting!");
+                return;
+            }
             window.location.href = "/broadcast/broadcaster-ui.html?broadcast=" + channel_desired;
         });
     });
